e24d9cf88ac06ca986635967154d882535406520
wjlee <000jun2626@gmail.com>
2024-12-26 06:24:22 +0900

    feat: 건물 프리팹 위치 초기화 및 스포너 스크립트 추가

---
+++ Kimchi-Run/Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject[] gameObjects; // 생성할 오브젝트들을 담을 배열.
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
